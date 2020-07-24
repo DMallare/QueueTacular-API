8265bfd6f5a9a560407ede789bf1f8aad51b2786
Danielle Mallare <mallare-dani.d@husky.neu.edu>
2020-07-23 20:37:07 -0700

    Creates addQueue mutation and resolver

--- api_handler.js
+++ api_handler.js
@@ -3,8 +3,31 @@ const db = require('./db.js');
 
 const resolvers = {
   Query: {
-    showQueue: (root, { id }) => db.queues.get(id),
+    showQueue: (_, { id }) => db.queues.get(id),
   },
+
+  Mutation: {
+    addQueue: (_, { title, status, description }) => {
+      const newID = db.queues.create({});
+      db.queues.update({
+        id: newID, status, title, description,
+      });
+      return db.queues.get(newID);
+    },
+    /*
+    addItem: (_, { item }) => {
+      db.queues.update({
+        id: item.id,
+        status: item.status,
+        // person: item.person,
+        description: item.description,
+      });
+      const updatedQueue = db.queues.get(item.id);
+      return updatedQueue;
+    },
+    */
+  },
+
 };
 
 module.exports = { resolvers };
