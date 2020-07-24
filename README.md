e46d2065ccd500c1c6fbbaf1a59422dd027bf640
Danielle Mallare <mallare-dani.d@husky.neu.edu>
2020-07-24 14:30:21 -0700

    Updating schema to reflect APIs that need to be implemented

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # GroupProject-SpaceForce-API
+
+
+### Queries
+
+#### Add to a queue
+mutation {
+  addQueue(newQueue:{
+    owner: "Ellie",
+    title: "Ellie's office hours",
+    description: "CS 5006 office hours",
+    }){
+      id
+      title
+      owner
+      description
+      status
+  }
+ 
+}
+
+#### show a queue
+query showQueue($id: Int!) {
+  showQueue(id: $id) {
+    title
+    items {
+      name
+    }  
+  }
+}
+
+
--- queue.js
+++ queue.js
@@ -36,30 +36,4 @@ async function addQueue(_, { newQueue }) {
   return savedQueue;
 }
 
-/*
-  Mutation: {
-    addQueue: (_, { title, status, description }) => {
-      const newID = db.queues.create({});
-      db.queues.update({
-        id: newID, status, title, description,
-      });
-      return db.queues.get(newID);
-    },
-    /*
-    addItem: (_, { item }) => {
-      db.queues.update({
-        id: item.id,
-        status: item.status,
-        // person: item.person,
-        description: item.description,
-      });
-      const updatedQueue = db.queues.get(item.id);
-      return updatedQueue;
-    },
-
-  },
-
-};
-*/
-
 module.exports = { showQueue, showAll, addQueue };
