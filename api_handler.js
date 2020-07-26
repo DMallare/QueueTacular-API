89885a516f52ebe589c5309ed30e5eb0dbe4a945
Danielle Mallare <mallare-dani.d@husky.neu.edu>
2020-07-26 02:31:06 -0700

    Resolves conflicts in api_handler

--- api_handler.js
+++ api_handler.js
@@ -9,12 +9,9 @@ const resolvers = {
   Mutation: {
     addQueue: queue.addQueue,
     queueUpdate: queue.queueUpdate,
-<<<<<<< HEAD
     itemUpdate: queue.itemUpdate,
-=======
     deleteQueue: queue.deleteQueue,
     deleteQueueItem: queue.deleteQueueItem,
->>>>>>> 823f8657cf4f540726891bd70979c762d6589f3c
   },
 };
 
