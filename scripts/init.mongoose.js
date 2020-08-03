9a2efaa7cfbb034f760a2cf80528662629632fd1
Danielle Mallare <mallare-dani.d@husky.neu.edu>
2020-08-03 13:25:39 -0700

    Adds more queues and items to init script

--- scripts/init.mongoose.js
+++ scripts/init.mongoose.js
@@ -15,7 +15,7 @@ Queue.collection.drop().then(
   },
   (err) => {
     console.log(err);
-  },
+  }
 );
 
 Item.collection.drop().then(
@@ -24,7 +24,7 @@ Item.collection.drop().then(
   },
   (err) => {
     console.log(err);
-  },
+  }
 );
 
 User.collection.drop().then(
@@ -33,7 +33,7 @@ User.collection.drop().then(
   },
   (err) => {
     console.log(err);
-  },
+  }
 );
 
 const David = new User({
@@ -60,8 +60,8 @@ const Winnie = new User({
   signedin: false,
 });
 
-const Winner = new User({
-  username: 'winner',
+const Tim = new User({
+  username: 'timmm',
   email: 'winerwinner@chickendinner.com',
   signedin: true,
 });
@@ -72,7 +72,19 @@ const Danielle = new User({
   signedin: false,
 });
 
-const userDB = [David, MKBHD, Billie, Winnie, Winner, Danielle];
+const Zack = new User({
+  username: 'zack.k',
+  email: 'zack.k@gmail.com',
+  signedin: true,
+});
+
+const Nachiket = new User({
+  username: 'nachi',
+  email: 'nachi1234@gmail.com',
+  signedin: true,
+});
+
+const userDB = [David, MKBHD, Billie, Winnie, Tim, Danielle, Zack, Nachiket];
 
 const itemOne = new Item({
   status: 'Removed',
@@ -97,12 +109,89 @@ const itemThree = new Item({
 
 const itemFour = new Item({
   status: 'Complete',
-  user: Winner,
+  user: Tim,
   wait: 0,
   description: 'queue item description four',
 });
 
-const itemDB = [itemOne, itemTwo, itemThree, itemFour];
+const itemFive = new Item({
+  status: 'Waiting',
+  user: Zack,
+  wait: 60,
+  description: 'waiting to get into Disneyland',
+});
+
+const itemSix = new Item({
+  status: 'Waiting',
+  user: Danielle,
+  wait: 11,
+  description: 'I need help with programming stuff',
+});
+
+const itemSeven = new Item({
+  status: 'Serving',
+  user: Nachiket,
+  wait: 30,
+  description: 'Working on A7',
+});
+
+const itemEight = new Item({
+  status: 'Complete',
+  user: Nachiket,
+  wait: 101,
+  description: 'Trying to get into disney...',
+});
+
+const itemNine = new Item({
+  status: 'Waiting',
+  user: Zack,
+  wait: 20,
+  description: 'Need help with Java',
+});
+
+const itemTen = new Item({
+  status: 'Waiting',
+  user: Danielle,
+  wait: 40,
+  description: 'Need help with making a hash table',
+});
+
+const itemEleven = new Item({
+  status: 'Waiting',
+  user: Tim,
+  wait: 50,
+  description: 'Need help with pointers',
+});
+
+const itemTwelve = new Item({
+  status: 'Serving',
+  user: Billie,
+  wait: 65,
+  description: 'Seg fault.',
+});
+
+const itemThirteen = new Item({
+  status: 'Completed',
+  user: Winnie,
+  wait: 15,
+  description: 'Program is crashing',
+});
+
+const itemDB = [
+  itemOne,
+  itemTwo,
+  itemThree,
+  itemFour,
+  itemFive,
+  itemSix,
+  itemSeven,
+  itemEight,
+  itemNine,
+  itemTen,
+  itemEleven,
+  itemTwelve,
+  itemThirteen,
+];
 
 const Doctor = new Queue({
   title: "Doctor's visit",
@@ -123,10 +212,30 @@ const Disneyland = new Queue({
     'This is the general ticketing queue for Disneyland - no fast passes. The lines for all rides are too long!',
   maxParticipants: 1000,
   maxWaitTime: 999,
-  items: [itemThree, itemFour],
+  items: [itemThree, itemFour, itemFive, itemEight],
 });
 
-const queueDB = [Doctor, Disneyland];
+const TimOH = new Queue({
+  title: "Tim's office hours",
+  status: 'Open',
+  owner: Tim,
+  description: 'Get your computer science questions answered!',
+  maxParticipants: 10,
+  maxWaitTime: 90,
+  items: [itemSix, itemSeven, itemNine],
+});
+
+const ZackOH = new Queue({
+  title: "Zack's office hours",
+  status: 'Open',
+  owner: Zack,
+  description: 'CS5006 Office hours',
+  maxParticipants: 10,
+  maxWaitTime: 110,
+  items: [itemTen, itemEleven, itemTwelve, itemThirteen],
+});
+
+const queueDB = [Doctor, Disneyland, TimOH, ZackOH];
 
 User.insertMany(userDB, (err) => {
   if (err) {
